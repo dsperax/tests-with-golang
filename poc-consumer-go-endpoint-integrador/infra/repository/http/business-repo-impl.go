198f3864a21071913eb900239ff2acb7437dfdd1
Diogo Sperandio <76974801+dsperax@users.noreply.github.com>
2023-11-11 13:58:41 -0300

    remove empity fields on JSON

--- poc-consumer-go-endpoint-integrador/infra/repository/http/business-repo-impl.go
+++ poc-consumer-go-endpoint-integrador/infra/repository/http/business-repo-impl.go
@@ -28,7 +28,13 @@ func (r *BusinessRepoHttp) EndPointRegras(obj domain.BusinessMessage) error {
 		return err
 	}
 
-	request, _ := http.NewRequest("POST", httpposturl, bytes.NewBuffer(jsonData))
+	data, err := removeEmptyFields(jsonData)
+	if err != nil {
+		log.GravarLog("Erro a remover campos vazios do JSON! " + err.Error())
+		return err
+	}
+	
+	request, _ := http.NewRequest("POST", httpposturl, bytes.NewBuffer(data))
 	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
 
 	client := &http.Client{}
@@ -41,4 +47,67 @@ func (r *BusinessRepoHttp) EndPointRegras(obj domain.BusinessMessage) error {
 
 	log.GravarLog("Response Status: " + response.Status)
 	return nil
+}
+
+func removeEmptyFields(data []byte) ([]byte, error) {
+	var obj interface{}
+	if err := json.Unmarshal(data, &obj); err != nil {
+		return nil, err
+	}
+
+	cleanedData := removeEmpty(obj)
+
+	result, err := json.Marshal(cleanedData)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func removeEmpty(data interface{}) interface{} {
+	switch v := data.(type) {
+	case map[string]interface{}:
+		cleaned := make(map[string]interface{})
+		for key, value := range v {
+			if value != nil {
+				switch value := value.(type) {
+				case string:
+					if value != "" {
+						cleaned[key] = removeEmpty(value)
+					}
+				case map[string]interface{}:
+					cleanedValue := removeEmpty(value)
+					if len(cleanedValue.(map[string]interface{})) > 0 {
+						cleaned[key] = cleanedValue
+					}
+				default:
+					cleaned[key] = removeEmpty(value)
+				}
+			}
+		}
+		return cleaned
+	case []interface{}:
+		cleaned := make([]interface{}, 0)
+		for _, value := range v {
+			if value != nil {
+				switch value := value.(type) {
+				case string:
+					if value != "" {
+						cleaned = append(cleaned, removeEmpty(value))
+					}
+				case map[string]interface{}:
+					cleanedValue := removeEmpty(value)
+					if len(cleanedValue.(map[string]interface{})) > 0 {
+						cleaned = append(cleaned, cleanedValue)
+					}
+				default:
+					cleaned = append(cleaned, removeEmpty(value))
+				}
+			}
+		}
+		return cleaned
+	default:
+		return v
+	}
 }
